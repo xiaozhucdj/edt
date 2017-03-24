c84184a8ad7e9635192422ce5f3315411cd0f3b6
fuhan <515073770@qq.com>
2017-03-24 11:27:09 +0800

    更新接口host地址

--- edt/youngyedu/src/main/java/com/yougy/common/global/Commons.java
+++ edt/youngyedu/src/main/java/com/yougy/common/global/Commons.java
@@ -18,7 +18,7 @@ public class Commons {
     /**
      * 外网接口
      */
-    private static final String URL = " http://106.15.45.14:8080/leke_api/android";
+    private static final String URL = "http://api.learningpad.cn:8080/leke_api/android";
 
     public static final String SHOP_URL = "http://192.168.12.7:80/api/bookStore";
     /**
