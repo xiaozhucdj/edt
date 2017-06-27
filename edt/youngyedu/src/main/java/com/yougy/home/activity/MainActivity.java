39415bc440c582eb09392eec0a0aad74016cc980
yuanye <jl_qiqi@163.com>
2017-06-27 09:47:59 +0800

    修改课外书显示吐司为对话框

--- edt/youngyedu/src/main/java/com/yougy/home/activity/MainActivity.java
+++ edt/youngyedu/src/main/java/com/yougy/home/activity/MainActivity.java
@@ -969,18 +969,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onUiDetermineListener() {
         super.onUiDetermineListener();
-        Intent intent = new Intent("android.intent.action.WIFI_ENABLE");
-        startActivity(intent);
-    }
-
-    @Override
-    public void onUiCancelListener() {
-        super.onUiCancelListener();
-    }
-
-    @Override
-    public void onUiCenterDetermineListener() {
-        super.onUiCenterDetermineListener();
+        jumpTonet();
     }
 }
 
