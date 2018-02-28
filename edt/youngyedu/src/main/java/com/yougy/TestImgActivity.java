298c5aa0fbbb48eb946708e91764911b570a9b85
jiangliang <jiangliang@youngyedu.com>
2018-02-28 18:04:27 +0800

    update

---
+++ edt/youngyedu/src/main/java/com/yougy/TestImgActivity.java
@@ -0,0 +1,20 @@
+package com.yougy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.yougy.ui.activity.R;
+
+/**
+ * Created by jiangliang on 2018-2-28.
+ */
+
+public class TestImgActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_img_layout);
+    }
+}
