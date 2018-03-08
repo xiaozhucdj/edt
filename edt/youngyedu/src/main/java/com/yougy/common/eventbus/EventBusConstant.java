17702ef99b884d5999e615b5c8a7c33bcf2103ec
yuanye <jl_qiqi@163.com>
2018-03-08 10:29:11 +0800

    学生端问答

--- edt/youngyedu/src/main/java/com/yougy/common/eventbus/EventBusConstant.java
+++ edt/youngyedu/src/main/java/com/yougy/common/eventbus/EventBusConstant.java
@@ -42,4 +42,6 @@ public class EventBusConstant {
     public static final String DELETE_PHOTO_EVENT = "delete_photo_event";
     public static final String  add_note = "add_note";
     public static final String  need_refresh = "need_refresh";
+    public static final String  answer_event = "answer_event";
+
 }
