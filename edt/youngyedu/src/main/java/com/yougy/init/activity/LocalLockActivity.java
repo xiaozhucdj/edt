1bd2e97d9d8262713cbc7bafc349710fff92e14b
yuanye <jl_qiqi@163.com>
2018-03-12 14:06:41 +0800

    适配初始化

--- edt/youngyedu/src/main/java/com/yougy/init/activity/LocalLockActivity.java
+++ edt/youngyedu/src/main/java/com/yougy/init/activity/LocalLockActivity.java
@@ -3,13 +3,20 @@ package com.yougy.init.activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.BatteryManager;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.yougy.common.activity.BaseActivity;
+import com.yougy.common.eventbus.BaseEvent;
+import com.yougy.common.eventbus.EventBusConstant;
+import com.yougy.common.manager.NetManager;
+import com.yougy.common.manager.PowerManager;
 import com.yougy.common.service.DownloadService;
+import com.yougy.common.utils.DateUtils;
 import com.yougy.common.utils.SpUtil;
 import com.yougy.common.utils.UIUtils;
 import com.yougy.home.activity.MainActivity;
@@ -19,6 +26,8 @@ import com.yougy.ui.activity.databinding.ActivityLocalLockBinding;
 import com.yougy.view.dialog.ConfirmDialog;
 import com.yougy.view.dialog.HintDialog;
 
+import de.greenrobot.event.EventBus;
+
 /**
  * Created by FH on 2017/6/22.
  */
@@ -27,21 +36,88 @@ public class LocalLockActivity extends BaseActivity {
     static public final String NOT_GOTO_HOMEPAGE_ON_ENTER = "not_goto_homepage";
     ActivityLocalLockBinding binding;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
+        NetManager.getInstance().registerReceiver(this);
+        PowerManager.getInstance().registerReceiver(this);
+    }
+
     @Override
     protected void setContentView() {
-        binding = DataBindingUtil.inflate(LayoutInflater.from(this) , R.layout.activity_local_lock , null , false);
+        binding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_local_lock, null, false);
         UIUtils.recursiveAuto(binding.getRoot());
         setContentView(binding.getRoot());
     }
 
-    @Override
-    public void init() {
+    private void initSysIcon() {
+        setSysWifi();
+        setSysTime();
+        setSysPower(PowerManager.getInstance().getlevelPercent(), PowerManager.getInstance().getBatteryStatus());
+    }
+
+    private void setSysTime() {
+        binding.tvTime.setText(DateUtils.getTimeHHMMString());
+
+    }
+
+    private void setSysWifi() {
+        if (NetManager.getInstance().isWifiConnected(this)) {
+            int level = NetManager.getInstance().getConnectionInfoRssi(this);
+            // 这个方法。得到的值是一个0到-100的区间值，是一个int型数据，其中0到-50表示信号最好，-50到-70表示信号偏差，
+            //小于-70表示最差，有可能连接不上或者掉线，一般Wifi已断则值为-200。
+            if (level <= 0 && level >= -50) {
+                binding.imgWifi.setImageDrawable(UIUtils.getDrawable(R.drawable.img_wifi_0));
+            } else if (level < -50 && level >= -70) {
+                binding.imgWifi.setImageDrawable(UIUtils.getDrawable(R.drawable.img_wifi_4));
+            } else if (level < -70 && level >= -80) {
+                binding.imgWifi.setImageDrawable(UIUtils.getDrawable(R.drawable.img_wifi_3));
+            } else if (level < -80 && level >= -100) {
+                binding.imgWifi.setImageDrawable(UIUtils.getDrawable(R.drawable.img_wifi_2));
+            }
+        } else {
+            binding.imgWifi.setImageDrawable(UIUtils.getDrawable(R.drawable.img_wifi_1));
+        }
+    }
+
+    private void setSysPower(int level, int state) {
+
+        binding.tvPower.setText(level + "%");
+        if (level == 0) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_0_black_03 : R.drawable.ic_battery_0_black_03));
+
+        } else if (level > 0 && level <= 10) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_10_black_03 : R.drawable.ic_battery_10_black_03));
+        } else if (level > 10 && level <= 20) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_20_black_03 : R.drawable.ic_battery_20_black_03));
+        } else if (level > 20 && level <= 30) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_30_black_03 : R.drawable.ic_battery_30_black_03));
+        } else if (level > 30 && level <= 40) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_40_black_03 : R.drawable.ic_battery_40_black_03));
+        } else if (level > 40 && level <= 50) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_50_black_03 : R.drawable.ic_battery_50_black_03));
+        } else if (level > 50 && level <= 60) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_60_black_03 : R.drawable.ic_battery_60_black_03));
+        } else if (level > 60 && level <= 70) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_70_black_03 : R.drawable.ic_battery_70_black_03));
+        } else if (level > 70 && level <= 80) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_80_black_03 : R.drawable.ic_battery_80_black_03));
+        } else if (level > 80 && level < 100) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(state == BatteryManager.BATTERY_STATUS_CHARGING ? R.drawable.ic_battery_charge_90_black_03 : R.drawable.ic_battery_90_black_03));
+        } else if (level == 100) {
+            binding.imgElectricity.setImageDrawable(UIUtils.getDrawable(R.drawable.ic_battery_100_black_03));
+        }
 
     }
 
     @Override
-    protected void initLayout() {
+    public void init() {
+    }
 
+    @Override
+    protected void initLayout() {
     }
 
     @Override
@@ -51,6 +127,7 @@ public class LocalLockActivity extends BaseActivity {
         binding.schoolTv.setText("学校 : " + student.getSchoolName());
         binding.classTv.setText("班级 : " + student.getClassName());
         binding.numTv.setText("编号 : " + student.getUserNum());
+        initSysIcon();
     }
 
     @Override
@@ -58,25 +135,24 @@ public class LocalLockActivity extends BaseActivity {
 
     }
 
-    public void enter(View view){
-        if (TextUtils.isEmpty(binding.localLockEdittext.getText())){
-            new HintDialog(getThisActivity() , "密码不能为空").show();
+    public void enter(View view) {
+        if (TextUtils.isEmpty(binding.localLockEdittext.getText())) {
+            new HintDialog(getThisActivity(), "密码不能为空").show();
             return;
         }
-        Log.v("FH" , "edittext : " + binding.localLockEdittext.getText().toString() + " local : " + SpUtil.getLocalLockPwd());
-        if (binding.localLockEdittext.getText().toString().equals(SpUtil.getLocalLockPwd())){
+        Log.v("FH", "edittext : " + binding.localLockEdittext.getText().toString() + " local : " + SpUtil.getLocalLockPwd());
+        if (binding.localLockEdittext.getText().toString().equals(SpUtil.getLocalLockPwd())) {
             finish();
-            if (!getIntent().getBooleanExtra(NOT_GOTO_HOMEPAGE_ON_ENTER, false)){
+            if (!getIntent().getBooleanExtra(NOT_GOTO_HOMEPAGE_ON_ENTER, false)) {
                 startService(new Intent(this, DownloadService.class));
                 loadIntent(MainActivity.class);
             }
-        }
-        else {
-            new HintDialog(getThisActivity() , "密码不正确").show();
+        } else {
+            new HintDialog(getThisActivity(), "密码不正确").show();
         }
     }
 
-    public void forgetPwd(View view){
+    public void forgetPwd(View view) {
         new ConfirmDialog(this, "忘记本机锁密码需要使用乐课账户密码重新登录来重置本机锁密码,是否确定要重置?"
                 , new DialogInterface.OnClickListener() {
             @Override
@@ -90,5 +166,32 @@ public class LocalLockActivity extends BaseActivity {
 
     @Override
     public void onBackPressed() {
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+        NetManager.getInstance().unregisterReceiver(this);
+        PowerManager.getInstance().unregisterReceiver(this);
+    }
+
+
+    public void wifi(View view){
+        boolean isConnected = NetManager.getInstance().isWifiConnected(this);
+        NetManager.getInstance().changeWiFi(this, !isConnected);
+        binding.imgWifi.setImageDrawable(UIUtils.getDrawable(isConnected ? R.drawable.img_wifi_1 : R.drawable.img_wifi_0));
+    }
+
+
+    public void onEventMainThread(BaseEvent event) {
+        if (event == null)
+            return;
+        setSysTime();
+        if (EventBusConstant.EVENT_WIIF.equals(event.getType())) {
+            setSysWifi();
+        } else if (EventBusConstant.EVENTBUS_POWER.equals(event.getType())) {
+            setSysPower(PowerManager.getInstance().getlevelPercent(), PowerManager.getInstance().getBatteryStatus());
+        }
     }
 }
