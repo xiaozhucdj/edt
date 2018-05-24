9ca820f788c2b9bc6d3496062db72d23baf3ae75
fuhan <515073770@qq.com>
2018-05-24 16:09:10 +0800

    fix bugs

--- edt/youngyedu/src/main/java/com/yougy/message/attachment/AskQuestionAttachment.java
+++ edt/youngyedu/src/main/java/com/yougy/message/attachment/AskQuestionAttachment.java
@@ -11,7 +11,7 @@ import org.json.JSONObject;
 public class AskQuestionAttachment extends CustomAttachment{
     final String KEY_EXAM_ID = "examId";
     final String KEY_ITEM_ID = "itemId";
-    final String KEY_FROM = "userId";
+    final String KEY_FROM = "examSponsor";
     final String KEY_QUESTION = "question";
     final String KEY_QUESTION_TYPE = "questionType";
 
@@ -31,10 +31,9 @@ public class AskQuestionAttachment extends CustomAttachment{
     @Override
     protected void parseData(JSONObject data) throws JSONException{
         JSONObject introJsonObj = data.getJSONObject(CustomAttachParser.KEY_INTRO);
-        JSONObject paramJsonObj = data.getJSONObject(CustomAttachParser.KEY_PARAM);
         examID = introJsonObj.getInt(KEY_EXAM_ID);
         itemId = introJsonObj.getInt(KEY_ITEM_ID);
-        from = paramJsonObj.getString(KEY_FROM);
+        from = introJsonObj.getString(KEY_FROM);
         questionType = introJsonObj.getJSONArray(KEY_QUESTION).getJSONObject(0).getString(KEY_QUESTION_TYPE);
     }
 
@@ -45,12 +44,11 @@ public class AskQuestionAttachment extends CustomAttachment{
     @Override
     protected JSONObject packData() {
         JSONObject returnJsonObj = new JSONObject();
-        JSONObject paramJsonObj = new JSONObject();
         JSONObject introJsonObj = new JSONObject();
         try {
             introJsonObj.put(KEY_EXAM_ID , examID);
             introJsonObj.put(KEY_ITEM_ID , itemId);
-            paramJsonObj.put(KEY_FROM , from);
+            introJsonObj.put(KEY_FROM , from);
 
             JSONObject tempObj = new JSONObject();
             tempObj.put(KEY_QUESTION_TYPE , questionType);
@@ -59,7 +57,6 @@ public class AskQuestionAttachment extends CustomAttachment{
             introJsonObj.put(KEY_QUESTION , tempArray);
 
             returnJsonObj.put(CustomAttachParser.KEY_INTRO , introJsonObj);
-            returnJsonObj.put(CustomAttachParser.KEY_PARAM , paramJsonObj);
         }
         catch (JSONException e) {
             e.printStackTrace();
