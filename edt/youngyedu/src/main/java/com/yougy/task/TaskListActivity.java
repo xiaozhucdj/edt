a789e3c1ab21603b8bfdfe1f46ac91a3de42a106
jiangliang <jiangliang@youngyedu.com>
2018-10-23 14:13:40 +0800

    add task package

---
+++ edt/youngyedu/src/main/java/com/yougy/task/TaskListActivity.java
@@ -0,0 +1,30 @@
+package com.yougy.task;
+
+import com.yougy.common.activity.BaseActivity;
+
+public class TaskListActivity extends BaseActivity {
+    @Override
+    protected void setContentView() {
+        
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initLayout() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void refreshView() {
+
+    }
+}
