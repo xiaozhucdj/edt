4cf5724aa8ae3ee6f3ed51e4d4691e4c0e543d45
yuanye <jl_qiqi@163.com>
2019-02-25 15:14:32 +0800

    pre v21 - v3

--- edt/youngyedu/src/main/java/com/yougy/common/manager/YoungyUncaughtExceptionHandler.java
+++ edt/youngyedu/src/main/java/com/yougy/common/manager/YoungyUncaughtExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.yougy.common.manager;
 
 import android.content.Context;
+import android.content.Intent;
 
 import com.yougy.common.activity.BaseActivity;
 import com.yougy.common.utils.FileUtils;
@@ -8,6 +9,7 @@ import com.yougy.common.utils.FormatUtils;
 import com.yougy.common.utils.LogUtils;
 import com.yougy.common.utils.PackageUtils;
 import com.yougy.common.utils.SystemUtils;
+import com.yougy.home.activity.SplashActivity;
 
 import java.io.File;
 import java.io.PrintWriter;
@@ -104,10 +106,10 @@ public class YoungyUncaughtExceptionHandler implements Thread.UncaughtExceptionH
                 } catch (Exception e) {
                     LogUtils.e(e);
                 } finally {
-                 /*   Intent intent = new Intent(YoungyApplicationManager.getContext(),SplashActivity.class);
+                   Intent intent = new Intent(YoungyApplicationManager.getContext(),SplashActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    YoungyApplicationManager.getContext().startActivity(intent);*/
-                    BaseActivity.finishAll();
+                    YoungyApplicationManager.getContext().startActivity(intent);
+                    LogUtils.e("ExceptionHandler", "finally");
                     android.os.Process.killProcess(android.os.Process.myPid());  //结束进程之前可以把你程序的注销或者退出代码放在这
                 }
             }
